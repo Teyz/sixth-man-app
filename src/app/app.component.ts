5b8595d981c563f76c33728f8bb6f88083cde6c2
Teyz <contact.teyz@gmail.com>
2020-03-15 19:59:35 +0100

    Amélioration Front page Login & Register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'sixth-man';
   version = 'Sixth Man version 1.0';
+
+  // getBackgroundColor(){
+  //   const color = 'orange';
+  //   return color;
+  // }
 }
