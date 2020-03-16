d60db5fd6dd07d403bcd808abddd616f688901d5
Teyz <contact.teyz@gmail.com>
2020-03-16 10:13:27 +0100

    test

---
+++ server.js
@@ -0,0 +1,27 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/sixth-man'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/sixth-man/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://sixth-man-app.herokuapp.com/");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header(
+      "Access-Control-Allow-Header",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+next();
+});
+  
